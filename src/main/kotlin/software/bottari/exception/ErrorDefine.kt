23445193a19285c26857000c32b6765cf89f634a
ansungwoo <asw4ever@naver.com>
2024-12-13 16:36:36 +0900

    feat: ErrorDefine 추가

--- src/main/kotlin/software/bottari/exception/ErrorDefine.kt
+++ src/main/kotlin/software/bottari/exception/ErrorDefine.kt
@@ -8,12 +8,16 @@ enum class ErrorDefine(
     val message: String
 ) {
 
-
-
     // NOT FOUND
-    USER_NOT_FOUND("4040",HttpStatus.NOT_FOUND, "회원이 존재하지 않습니다."),
-    INVALID_CREDENTIALS("4041",HttpStatus.NOT_FOUND, "비밀번호가 틀렸습니다."),
-    // Bad Request
-    DUPLICATE_USER_EMAIL("4001", HttpStatus.BAD_REQUEST, "이메일이 중복됩니다."),
-    INVALID_ARGUMENT("4000", HttpStatus.BAD_REQUEST, "요청이 잘못되었습니다. ");
+    USER_NOT_FOUND("4040", HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+    INVALID_CREDENTIALS("4041", HttpStatus.NOT_FOUND, "비밀번호가 올바르지 않습니다."),
+
+    // BAD REQUEST
+    INVALID_FORMAT("4003", HttpStatus.BAD_REQUEST, "입력 값의 형식이 잘못되었습니다."),
+    MISSING_DOCUMENT("4004", HttpStatus.BAD_REQUEST, "필수 서류가 누락되었습니다."),
+    INVALID_SHIP_SCHEDULE_ID("4002", HttpStatus.BAD_REQUEST, "존재하지 않는 ShipScheduleId입니다."),
+    DUPLICATE_USER_EMAIL("4001", HttpStatus.BAD_REQUEST, "이미 사용 중인 이메일입니다."),
+    INVALID_ARGUMENT("4000", HttpStatus.BAD_REQUEST, "잘못된 요청입니다.");
+
 }
+
