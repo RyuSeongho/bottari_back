41a402b7d72fc30700366ce3d324d1aafc17e82b
HyundongdongYun <my4719cuswo12@gmail.com>
2024-12-15 05:02:18 +0900

    Feat : Reservation Controller, Service

---
+++ src/main/kotlin/software/bottari/controller/ReservationController.kt
@@ -0,0 +1,22 @@
+package software.bottari.controller
+
+import lombok.RequiredArgsConstructor
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import software.bottari.dto.request.ReservationRequestDto
+import software.bottari.dto.response.ReservationResponseDto
+import software.bottari.dto.response.ResponseDto
+import software.bottari.service.ReservationService
+
+@RequiredArgsConstructor
+@RestController
+class ReservationController (
+    private val reservationService: ReservationService
+) {
+
+    @PostMapping("/domestic")
+    fun domestic(@RequestBody reservationRequestDto: ReservationRequestDto): ResponseDto<ReservationResponseDto> {
+        return ResponseDto(reservationService.domestic(reservationRequestDto))
+    }
+}
