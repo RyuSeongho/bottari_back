daae174784cfd21e2ba42ddba97fc892a2ebc7cd
HyundongdongYun <my4719cuswo12@gmail.com>
2024-12-15 05:01:39 +0900

    Feat : Reservation Dto

---
+++ src/main/kotlin/software/bottari/dto/request/ReservationRequestDto.kt
@@ -0,0 +1,29 @@
+package software.bottari.dto.request
+
+
+data class ReservationRequestDto(
+    val productType: String,
+    val productPrice: Long,
+    val productWeight: Float,
+    val senderName: String,
+    val clearanceNumber: String,
+    val remark: String,
+    val quantity: Int,
+    val senderAddress: String,
+    val senderNumber: String,
+    val receiverName: String,
+    val receiverAddress: String,
+    val receiverNumber: Int,
+    val receiverCountry : String,
+    val receiverCity : String,
+    val receiverState : String,
+    val stateReceiver : String,
+    val zipcodeReceiver : Int,
+    val shippingStatus: String,
+    val additionalDoc: String
+){
+    fun toEntity(): ReservationRequestDto {
+        return this.copy()
+    }
+
+}
