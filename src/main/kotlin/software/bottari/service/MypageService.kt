73633f6b639a6d76abe7ac711c3e6dc50dd590e8
HyundongdongYun <my4719cuswo12@gmail.com>
2024-12-16 04:05:38 +0900

    Feat : add inquiryList, status

--- src/main/kotlin/software/bottari/service/MypageService.kt
+++ src/main/kotlin/software/bottari/service/MypageService.kt
@@ -1,21 +1,24 @@
 package software.bottari.service
 
 import lombok.RequiredArgsConstructor
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
+import software.bottari.domain.DeliveryStatus
 import software.bottari.domain.Inquiry
 import software.bottari.dto.request.InquiryRequestDto
-import software.bottari.dto.response.GetInquiryResponseDto
-import software.bottari.dto.response.InquiryResponseDto
+import software.bottari.dto.response.*
 import software.bottari.exception.ApiException
 import software.bottari.exception.ErrorDefine
-import software.bottari.repository.MemberRepository
-import software.bottari.repository.InquiryRepository
+import software.bottari.repository.*
 
 @Service
 @RequiredArgsConstructor
-class MypageService (
+class MypageService(
     private val memberRepository: MemberRepository,
-    private val inquiryRepository: InquiryRepository
+    private val inquiryRepository: InquiryRepository,
+    private val contractRepository: ContractRepository,
+    private val deliveryStatusRepository: DeliveryStatusRepository,
+    private val trackingInfoRepository: TrackingInfoRepository
 
 ){
     fun inquiry(inquiryRequestDto: InquiryRequestDto): InquiryResponseDto {
@@ -35,4 +38,39 @@ class MypageService (
             .orElseThrow{ ApiException(ErrorDefine.INQUIRY_NOT_FOUND) }
         return GetInquiryResponseDto.of(inquiry)
     }
+
+    fun getInquiryList(memberName: String, page: Int): InquiryListResponseDto {
+        val pageable = PageRequest.of(page, 10) // 페이지와 크기 설정
+        val inquiryPage = inquiryRepository.findAllByMemberName(memberName, pageable)
+
+        if (inquiryPage.isEmpty) {
+            throw ApiException(ErrorDefine.INQUIRY_NOT_FOUND)
+        }
+
+        val inquiriesDto = inquiryPage.content.map { inquiry ->
+            GetInquiryResponseDto.of(inquiry)
+        }
+        val pageInfo = PageInfoResponseDto.of(inquiryPage)
+
+        return InquiryListResponseDto.of(
+            inquiryList = inquiriesDto,
+            pageInfo = pageInfo
+        )
+    }
+
+    fun getStatus(orderId: Long): StatusResponseDto {
+        val deliveryStatus = deliveryStatusRepository.findByContractId(orderId)
+            .orElseThrow{ ApiException(ErrorDefine.CONTRACT_NOT_FOUND)}
+
+        val trackingInfos = trackingInfoRepository.findAllByDeliveryStatus(deliveryStatus)
+        val trackingList = trackingInfos.map{ trackingInfo ->
+            TrackingInfoResponseDto.of(trackingInfo)
+        }
+        val deliveryInfo = DeliveryStatusResponseDto.of(deliveryStatus)
+        return StatusResponseDto.of(
+            trackingList = trackingList,
+            deliveryInfo = deliveryInfo
+        )
+    }
+
 }
