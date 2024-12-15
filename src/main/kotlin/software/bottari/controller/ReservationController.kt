8ac7a628b8571780210c40aa820c7f82d9e22905
HyundongdongYun <my4719cuswo12@gmail.com>
2024-12-15 15:40:49 +0900

    Feat : add url

--- src/main/kotlin/software/bottari/controller/ReservationController.kt
+++ src/main/kotlin/software/bottari/controller/ReservationController.kt
@@ -3,6 +3,7 @@ package software.bottari.controller
 import lombok.RequiredArgsConstructor
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import software.bottari.dto.request.ReservationRequestDto
 import software.bottari.dto.response.ReservationResponseDto
@@ -10,6 +11,7 @@ import software.bottari.dto.response.ResponseDto
 import software.bottari.service.ReservationService
 
 @RequiredArgsConstructor
+@RequestMapping("reservation")
 @RestController
 class ReservationController (
     private val reservationService: ReservationService
