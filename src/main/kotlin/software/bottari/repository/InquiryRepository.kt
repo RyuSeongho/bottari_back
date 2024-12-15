e242118d0a18d6d4659a7b762b11803e1a5d0471
HyundongdongYun <my4719cuswo12@gmail.com>
2024-12-16 04:06:15 +0900

    Feat : modify InquiryRepository

--- src/main/kotlin/software/bottari/repository/InquiryRepository.kt
+++ src/main/kotlin/software/bottari/repository/InquiryRepository.kt
@@ -1,9 +1,15 @@
 package software.bottari.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import software.bottari.domain.Inquiry
-import java.util.Optional
 
 @Repository
-interface InquiryRepository : JpaRepository<Inquiry , Long>
+interface InquiryRepository : JpaRepository<Inquiry , Long> {
+    @Query("SELECT i FROM Inquiry i where i.member.name = :memberName")
+    fun findAllByMemberName(@Param("memberName")memberName: String, pageable: Pageable): Page<Inquiry>
+}
