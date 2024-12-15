34edc670c050f8e2204fe51b753b3c8461420631
HyundongdongYun <my4719cuswo12@gmail.com>
2024-12-15 15:40:12 +0900

    Feat : add url

--- src/main/kotlin/software/bottari/controller/MypageController.kt
+++ src/main/kotlin/software/bottari/controller/MypageController.kt
@@ -1,15 +1,15 @@
 package software.bottari.controller
 
 import lombok.RequiredArgsConstructor
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import software.bottari.dto.request.InquiryRequestDto
+import software.bottari.dto.response.GetInquiryResponseDto
 import software.bottari.dto.response.InquiryResponseDto
 import software.bottari.dto.response.ResponseDto
 import software.bottari.service.MypageService
 
 @RequiredArgsConstructor
+@RequestMapping("/mypage")
 @RestController
 class MypageController(
     private val mypageService: MypageService,
@@ -17,5 +17,10 @@ class MypageController(
     @PostMapping("/inquiry")
     fun inquiry(@RequestBody mypageRequestDto: InquiryRequestDto): ResponseDto<InquiryResponseDto> {
         return ResponseDto(mypageService.inquiry(mypageRequestDto))
+    }
+
+    @GetMapping("/inquiry")
+    fun getInquiry(@RequestParam(value = "inquiryId") inquiryId: Long): ResponseDto<GetInquiryResponseDto> {
+        return ResponseDto(mypageService.getInquiry(inquiryId))
     }
 }
