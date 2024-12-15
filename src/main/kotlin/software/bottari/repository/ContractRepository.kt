808ae441a9fd648c2e759f6f4efaa0ec894f2a08
HyundongdongYun <my4719cuswo12@gmail.com>
2024-12-16 05:04:24 +0900

    Feat : add getContractList

--- src/main/kotlin/software/bottari/repository/ContractRepository.kt
+++ src/main/kotlin/software/bottari/repository/ContractRepository.kt
@@ -1,8 +1,16 @@
 package software.bottari.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import software.bottari.domain.Contract
+import software.bottari.domain.Inquiry
 
 @Repository
-interface ContractRepository: JpaRepository<Contract, Long> {}
+interface ContractRepository: JpaRepository<Contract, Long> {
+    @Query("SELECT i FROM Contract i where i.member.name = :memberName")
+    fun findAllByMemberName(@Param("memberName")memberName: String, pageable: Pageable): Page<Contract>
+}
