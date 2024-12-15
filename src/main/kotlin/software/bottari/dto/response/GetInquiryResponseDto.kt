01974217e60bd90051f1223b242b14d1f201016e
HyundongdongYun <my4719cuswo12@gmail.com>
2024-12-16 04:07:26 +0900

    Feat : add answerStatus val

--- src/main/kotlin/software/bottari/dto/response/GetInquiryResponseDto.kt
+++ src/main/kotlin/software/bottari/dto/response/GetInquiryResponseDto.kt
@@ -5,14 +5,16 @@ import software.bottari.domain.Inquiry
 data class GetInquiryResponseDto (
     val title: String,
     val content: String,
-    val inquiryId: Long
+    val inquiryId: Long,
+    val answerStatus: Boolean
 ){
     companion object {
         fun of(inquiry: Inquiry): GetInquiryResponseDto {
             return GetInquiryResponseDto(
                 title = inquiry.title,
                 content = inquiry.content,
-                inquiryId = inquiry.id
+                inquiryId = inquiry.id,
+                answerStatus = inquiry.reply!=null
             )
         }
     }
